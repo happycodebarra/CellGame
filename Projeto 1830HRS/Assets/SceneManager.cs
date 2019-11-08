d8775a3a44f224ac2d249b4eeaf49919926ddd32
happycodebarra <53911823+happycodebarra@users.noreply.github.com>
2019-11-08 19:36:45 -0300

    Gerenciador de cenas
    Gerenciador de cenas, para gerenciar cenas

---
+++ Projeto 1830HRS/Assets/SceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GerenciadorDeCenas : MonoBehaviour
+{
+   void mudarCena(string nomeDaCena)
+    {
+        Scene.LoadScene(nomeDaCena); 
+    }
+    
+}
