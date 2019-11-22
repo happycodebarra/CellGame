cbf1a72e928c3c79c15ddc8f11cddb645db7f083
PC 4 <53911823+happycodebarra@users.noreply.github.com>
2019-11-22 20:02:16 -0300

    tt de cog

---
+++ Projeto 1830HRS/Assets/Script/ComportamentoCelular.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamentoCelular : MonoBehaviour
+{
+    public GameObject celula;
+
+    public GameObject nucleoQRCode;
+
+    public GameObject nucleoCelula;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(celula.transform.position, nucleoQRCode.transform.position) < 1.5)
+        {
+            nucleoCelula.SetActive(true); 
+            nucleoQRCode.SetActive(false);
+        }
+    }
+}
--- Projeto 1830HRS/Assets/Script/GerenciadorDeCenas.cs
+++ Projeto 1830HRS/Assets/Script/GerenciadorDeCenas.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
+
+
 public class GerenciadorDeCenas : MonoBehaviour
 {
    void mudarCena(string nomeDaCena)
     {
-        Scene.LoadScene(nomeDaCena); 
+        SceneManager.LoadScene(nomeDaCena); 
     }
     
 }
